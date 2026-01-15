ee3645da53bbc27af2e18f6868a60a49e00244ff
pavels.korzs <pavels.korzs@codex.lv>
2026-01-15 21:42:33 +0200

    task1

--- src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
+++ src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
@@ -1,28 +1,33 @@
 package com.fulfilment.application.monolith.location;
-
 import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import com.fulfilment.application.monolith.warehouses.domain.ports.LocationResolver;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped
 public class LocationGateway implements LocationResolver {
-
-  private static final List<Location> locations = new ArrayList<>();
-
+  private static final Map<String, Location> locations = new HashMap<>();
   static {
-    locations.add(new Location("ZWOLLE-001", 1, 40));
-    locations.add(new Location("ZWOLLE-002", 2, 50));
-    locations.add(new Location("AMSTERDAM-001", 5, 100));
-    locations.add(new Location("AMSTERDAM-002", 3, 75));
-    locations.add(new Location("TILBURG-001", 1, 40));
-    locations.add(new Location("HELMOND-001", 1, 45));
-    locations.add(new Location("EINDHOVEN-001", 2, 70));
-    locations.add(new Location("VETSBY-001", 1, 90));
+    addLocation("ZWOLLE-001", 1, 40);
+    addLocation("ZWOLLE-002", 2, 50);
+    addLocation("AMSTERDAM-001", 5, 100);
+    addLocation("AMSTERDAM-002", 3, 75);
+    addLocation("TILBURG-001", 1, 40);
+    addLocation("HELMOND-001", 1, 45);
+    addLocation("EINDHOVEN-001", 2, 70);
+    addLocation("VETSBY-001", 1, 90);
   }
 
   @Override
   public Location resolveByIdentifier(String identifier) {
-    // TODO implement this method
-    throw new UnsupportedOperationException("Unimplemented method 'resolveByIdentifier'");
+    return locations.get(identifier);
   }
+
+
+  private static void addLocation(String identification, int maxNumberOfWarehouses, int maxCapacity) {
+    locations.put(identification, new Location(identification, maxNumberOfWarehouses, maxCapacity));
+  }
+
 }
--- src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,25 @@
 package com.fulfilment.application.monolith.location;
 
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
+import com.fulfilment.application.monolith.warehouses.domain.ports.LocationResolver;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@QuarkusTest
 public class LocationGatewayTest {
+
+  @Inject
+  LocationResolver locationGateway;
 
   @Test
   public void testWhenResolveExistingLocationShouldReturn() {
-    // given
-    // LocationGateway locationGateway = new LocationGateway();
-
-    // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
-
-    // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+    Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    assertEquals("ZWOLLE-001", location.identification);
+    location = locationGateway.resolveByIdentifier("ZWOLLE-999");
+    assertNull(location);
   }
 }
